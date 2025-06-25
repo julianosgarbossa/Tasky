74766effe2c51505c5d00a015c8f0eeddba1f821
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-06-25 13:18:04 -0300

    Criação do Projeto

---
+++ Tasky/Tasky/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Tasky
+//
+//  Created by Juliano Sgarbossa on 25/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
