472d33166c175a83c35f20fa15915028ce530680
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-06-25 13:40:16 -0300

    Configurações Inicias
    Configurando projeto para funcionar via viewcode.
    
    Importante assets de imagens e cores.
    
    Criando um enum de constantes para utilizar imagens e cores globalmente no código.

---
+++ Tasky/Tasky/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Tasky
+//
+//  Created by Juliano Sgarbossa on 25/06/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(named: AssetsConstants.darkPurple)
+    }
+}
---
+++ Tasky/Tasky/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Tasky
+//
+//  Created by Juliano Sgarbossa on 25/06/25.
+//
+
+import Foundation
+
+enum AssetsConstants {
+    static let homeIllustration = "home-illustration"
+    static let tasksIllustration = "tasks-illustration"
+    static let logoIllustration = "logo-illustration"
+    static let darkPurple = "DarkPurple"
+    static let lilac = "Lilac"
+}
