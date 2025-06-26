7d229ec8f7f5c8e9232fdcd1725466888d3d4026
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-06-26 15:58:16 -0300

    UserDefaults
    Salvando o array de tasks (tarefas) no userDefaults.
    
    Carregando o array de tasks (tarefas) do userDefaults.

--- Tasky/Repository/TaskRepository.swift
+++ Tasky/Repository/TaskRepository.swift
@@ -9,21 +9,46 @@ import Foundation
 
 class TaskRepository {
     
-    init() {}
-    
     var tasks: [Task] = []
+    private let userDefaults = UserDefaults.standard
+    private let tasksKey = "tasky-app-tasks"
+    
+    init() {
+        self.loadTasks()
+    }
     
     func addTask(newTask: Task) {
         tasks.append(newTask)
+        self.saveTasks()
     }
     
     func removeTask(at index: Int) {
         guard index >= 0 && index < tasks.count else { return }
         tasks.remove(at: index)
+        self.saveTasks()
     }
     
     func completeTask(at index: Int) {
         guard index >= 0 && index < tasks.count else { return }
         tasks[index].isCompleted.toggle()
+        self.saveTasks()
+    }
+    
+    private func saveTasks() {
+        do {
+            let tasksData = try JSONEncoder().encode(tasks)
+            userDefaults.setValue(tasksData, forKey: tasksKey)
+        } catch {
+            print("ocorreu um erro ao salvar as tarefas: \(error)")
+        }
+    }
+    
+    private func loadTasks() {
+        guard let tasksData = userDefaults.data(forKey: tasksKey) else { return }
+        do {
+            tasks = try JSONDecoder().decode([Task].self, from: tasksData)
+        } catch {
+            print("ocorreu um erro ao carregar as tarefas: \(error)")
+        }
     }
 }
